f193a74866d629effa4690b05dfb649fdcfda4ad
Mohamed Shiyas B <147586484+Shiyas9961@users.noreply.github.com>
2024-03-06 12:11:28 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ function Header () {
 
     return (
         <header className="flex justify-between bg-primary items-center">
-            <a href="#" className="font-bold text-xl sm:text-2xl text-black md:text-3xl hover:scale-105 hover:text-white duration-300">Mohamed Shiyas B</a>
+            <a href="#summery" className="font-bold text-xl sm:text-2xl text-black md:text-3xl hover:scale-105 hover:text-white duration-300">Mohamed Shiyas B</a>
             <nav  className="hidden md:block duration-150">
                 <menu className="flex gap-4 text-white text-xl ">
-                    <li ><a className='hover:bg-[#2b2d77] hover:text-white duration-300 border p-2 rounded overflow-hidden' href="/">Home</a></li>
+                    <li ><a className='hover:bg-[#2b2d77] hover:text-white duration-300 border p-2 rounded overflow-hidden' href="#summery">Home</a></li>
                     <li ><a className='hover:bg-[#2b2d77] hover:text-white duration-300 border p-2 rounded overflow-hidden' href="#about">About</a></li>
                     <li ><a className='hover:bg-[#2b2d77] hover:text-white duration-300 border p-2 rounded overflow-hidden' href="#projects">Projects</a></li>
                     <li ><a className='hover:bg-[#2b2d77] hover:text-white duration-300 border p-2 rounded overflow-hidden' href="#resume">Resume</a></li>
@@ -21,7 +21,7 @@ function Header () {
                 shoeMenu && (
                     <nav className="block md:hidden">
                     <menu className="flex flex-col text-white text-xl mobile-nav">
-                        <li onClick={() => setShowMenu(false)}><a href="/">Home</a></li>
+                        <li onClick={() => setShowMenu(false)}><a href="#summery">Home</a></li>
                         <li onClick={() => setShowMenu(false)}><a href="#about">About</a></li>
                         <li onClick={() => setShowMenu(false)}><a href="#projects">Projects</a></li>
                         <li onClick={() => setShowMenu(false)}><a href="#resume">Resume</a></li>
